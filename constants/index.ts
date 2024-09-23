04edf4e94be332cfee34605adea2778bd70aca94
Shubham Ghimire <shubham0011ghimire@gmail.com>
2024-09-23 20:18:37 +0545

    ADD: Home Routing

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sideabarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imageUrl: '/icons/Home.svg',
+    },
+    {
+        label: 'Upcoming',
+        route: '/upcoming',
+        imageUrl: '/icons/upcoming.svg',
+    },
+    {
+        label: 'Previous',
+        route: '/previous',
+        imageUrl: '/icons/Previous.svg',
+    },
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imageUrl: '/icons/video.svg',
+    },
+    {
+        label: 'Personal Room',
+        route: '/personal-room',
+        imageUrl: '/icons/add-personal.svg',
+    },
+]
